c985d02276382594abefaa6728b05aa197c8362e
moravechynek <moravechynek@seznam.cz>
2021-04-13 16:36:38 +0200

    mapa

--- script.js
+++ script.js
@@ -1,10 +1,188 @@
 $(document).ready(function(){
-  $("#hide").click(function(){
-    $("p").hide();
+  // historie
+  $("#historie1").mouseover(function(){
+    $("#historie1").css("background-color", "beige");
+    $("#historie1 p").css("font-size", 18);
+    $("#historie1 h3").css("font-size", 30);
   });
-  $("#show").click(function(){
-    $("p").show();
+  $("#historie1").mouseout(function(){
+    $("#historie1").css("background-color", "white");
+    $("#historie1 p").css("font-size", 16);
+    $("#historie1 h3").css("font-size", 28);
   });
+  $("#historie2").mouseover(function(){
+    $("#historie2").css("background-color", "beige");
+    $("#historie2 p").css("font-size", 18);
+    $("#historie2 h3").css("font-size", 30);
+  });
+  $("#historie2").mouseout(function(){
+    $("#historie2").css("background-color", "white");
+    $("#historie2 p").css("font-size", 16);
+    $("#historie2 h3").css("font-size", 28);
+  });
+  $("#historie3").mouseover(function(){
+    $("#historie3").css("background-color", "beige");
+    $("#historie3 p").css("font-size", 18);
+    $("#historie3 h3").css("font-size", 30);
+  });
+  $("#historie3").mouseout(function(){
+    $("#historie3").css("background-color", "white");
+    $("#historie3 p").css("font-size", 16);
+    $("#historie3 h3").css("font-size", 28);
+  });
+  $("#historie4").mouseover(function(){
+    $("#historie4").css("background-color", "beige");
+    $("#historie4 p").css("font-size", 18);
+    $("#historie4 h3").css("font-size", 30);
+  });
+  $("#historie4").mouseout(function(){
+    $("#historie4").css("background-color", "white");
+    $("#historie4 p").css("font-size", 16);
+    $("#historie4 h3").css("font-size", 28);
+  });
+  $("#historie5").mouseover(function(){
+    $("#historie5").css("background-color", "beige");
+    $("#historie5 p").css("font-size", 18);
+    $("#historie5 h3").css("font-size", 30);
+  });
+  $("#historie5").mouseout(function(){
+    $("#historie5").css("background-color", "white");
+    $("#historie5 p").css("font-size", 16);
+    $("#historie5 h3").css("font-size", 28);
+  });
+  $("#historie6").mouseover(function(){
+    $("#historie6").css("background-color", "beige");
+    $("#historie6 p").css("font-size", 18);
+    $("#historie6 h3").css("font-size", 30);
+  });
+  $("#historie6").mouseout(function(){
+    $("#historie6").css("background-color", "white");
+    $("#historie6 p").css("font-size", 16);
+    $("#historie6 h3").css("font-size", 28);
+  });
+  // mapa
+  $("#manawatu_wanganui").mouseover(function(){
+    $("#manawatu_wanganui").attr("fill","green");
+  });
+  $("#manawatu_wanganui").mouseout(function(){
+    $("#manawatu_wanganui").attr("fill","#7c7c7c");
+  });
+  $("#gisborne_district").mouseover(function(){
+    $("#gisborne_district").attr("fill","green");
+  });
+  $("#gisborne_district").mouseout(function(){
+    $("#gisborne_district").attr("fill","#7c7c7c");
+  });
+  $("#hawkes_bay").mouseover(function(){
+    $("#hawkes_bay").attr("fill","green");
+  });
+  $("#hawkes_bay").mouseout(function(){
+    $("#hawkes_bay").attr("fill","#7c7c7c");
+  });
+  $("#auckland").mouseover(function(){
+    $("#auckland").attr("fill","green");
+  });
+  $("#auckland").mouseout(function(){
+    $("#auckland").attr("fill","#7c7c7c");
+  });
+  $("#bay_of_plenty").mouseover(function(){
+    $("#bay_of_plenty").attr("fill","green");
+  });
+  $("#bay_of_plenty").mouseout(function(){
+    $("#bay_of_plenty").attr("fill","#7c7c7c");
+  });
+  $("#canterbury").mouseover(function(){
+    $("#canterbury").attr("fill","green");
+  });
+  $("#canterbury").mouseout(function(){
+    $("#canterbury").attr("fill","#7c7c7c");
+  });
+  $("#marlborough_district").mouseover(function(){
+    $("#marlborough_district").attr("fill","green");
+  });
+  $("#marlborough_district").mouseout(function(){
+    $("#marlborough_district").attr("fill","#7c7c7c");
+  });
+  $("#nelson_city").mouseover(function(){
+    $("#nelson_city").attr("fill","green");
+  });
+  $("#nelson_city").mouseout(function(){
+    $("#nelson_city").attr("fill","#7c7c7c");
+  });
+  $("#tasman_district").mouseover(function(){
+    $("#tasman_district").attr("fill","green");
+  });
+  $("#tasman_district").mouseout(function(){
+    $("#tasman_district").attr("fill","#7c7c7c");
+  });
+  $("#northland").mouseover(function(){
+    $("#northland").attr("fill","green");
+  });
+  $("#northland").mouseout(function(){
+    $("#northland").attr("fill","#7c7c7c");
+  });
+  $("#taranaki").mouseover(function(){
+    $("#taranaki").attr("fill","green");
+  });
+  $("#taranaki").mouseout(function(){
+    $("#taranaki").attr("fill","#7c7c7c");
+  });
+  $("#west_coast").mouseover(function(){
+    $("#west_coast").attr("fill","green");
+  });
+  $("#west_coast").mouseout(function(){
+    $("#west_coast").attr("fill","#7c7c7c");
+  });
+  $("#otago").mouseover(function(){
+    $("#otago").attr("fill","green");
+  });
+  $("#otago").mouseout(function(){
+    $("#otago").attr("fill","#7c7c7c");
+  });
+  $("#southland").mouseover(function(){
+    $("#southland").attr("fill","green");
+  });
+  $("#southland").mouseout(function(){
+    $("#southland").attr("fill","#7c7c7c");
+  });
+  $("#waikato").mouseover(function(){
+    $("#waikato").attr("fill","green");
+  });
+  $("#waikato").mouseout(function(){
+    $("#waikato").attr("fill","#7c7c7c");
+  });
+  $("#wellington").mouseover(function(){
+    $("#wellington").attr("fill","green");
+  });
+  $("#wellington").mouseout(function(){
+    $("#wellington").attr("fill","#7c7c7c");
+  });
+// ggggggggg
+  $("#three_kings_islands").mouseover(function(){
+    $("#three_kings_islands").attr("fill","green");
+  });
+  $("#three_kings_islands").mouseout(function(){
+    $("#three_kings_islands").attr("fill","#7c7c7c");
+  });
+  $("#antiopodes_islands").mouseover(function(){
+    $("#antiopodes_islands").attr("fill","green");
+  });
+  $("#antiopodes_islands").mouseout(function(){
+    $("#antiopodes_islands").attr("fill","#7c7c7c");
+  });
+  $("#campbell_islands").mouseover(function(){
+    $("#campbell_islands").attr("fill","green");
+  });
+  $("#campbell_islands").mouseout(function(){
+    $("#campbell_islands").attr("fill","#7c7c7c");
+  });
+  $("#auckland_islands").mouseover(function(){
+    $("#auckland_islands").attr("fill","green");
+  });
+  $("#auckland_islands").mouseout(function(){
+    $("#auckland_islands").attr("fill","#7c7c7c");
+  });
+  /*$("#0").mouseover(function(){
+    $("#0").attr("fill","blue")
+  });*/
 });
-
-console.log("Hello");
